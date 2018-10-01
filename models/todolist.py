64fb7f49dcc6416b7139f46a2b2072db551d3810
Alejandro Sanchez <>
2018-10-01 19:08:46 -0400

    Se incorpora vista calendario

--- models/todolist.py
+++ models/todolist.py
@@ -13,17 +13,20 @@ class ToDoList(models.Model):
 	_rec_name = 'name'
 
 	name = fields.Char('Tarea', required=True)
+	date_start = fields.Date(string='Fecha de Inicio', help='Fecha en la que comienza una actividad')
 	date_end = fields.Date(string='Fecha de Finalizacion', help="Realizada en la Fecha Indicada por el usuario")
 	date_deadline = fields.Date(string='Fecha Limite', required=True, help="Fecha limite para culminar las tareas")
-	state = fields.Selection([('pendiente','Pendiente'),('resuelta','Resuelta')],string='Estado', default='pendiente')
+	state = fields.Selection([('training','Training'),('task','Task')],string='Estado', default='task')
 	observacion = fields.Text(string='observacion', help="Ingrese algun comentario sobre la tarea")
 	topics_ids = fields.One2many('formacion.plan.estudio', 'todo_id', string='Topics')
-	formacion = fields.Boolean(string='Training')
+	formacion = fields.Boolean(string='Training', default=False)
 
 
-	def formacion(self):
-		self.formacion = True
+	def func_formacion(self):
+		self.state = 'training'
 
+	def deshacer_training(self):
+		self.state = 'task'
 
 
 
@@ -49,6 +52,17 @@ class FormacionPlanEstudio(models.Model):
 	date_start = fields.Date(string='Fecha de Inicio', help="Fecha real en la cual se empieza a estudiar un tema")
 	date_end = fields.Date(string='Fecha de Termino', Help="Fecha de finalizacion de estudio de un tema")
 	todo_id = fields.Many2one('to.do.list', string='Task')
+	listo = fields.Boolean(string="Listo")
 
-# crear un modelo que me permita anotar para investigar un tema o algo 
-# que me llame la atencion para no olvidarlo e investigarlo luego
+
+
+class FormacionModuloObservacion(models.Model):
+	""" Observaciones sobre el desarrollo del Modulo """
+	_name = 'formacion.modulo.observacion'
+	#rec_name = ''
+	#descripcion = ''
+
+	observacion = fields.Text(string='Observacion')
+	date = fields.Date(string='Fecha')
+
+	
